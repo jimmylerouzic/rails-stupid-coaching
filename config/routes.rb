f3d20d356ca6f57edb5e280795a7128b539d22a8
Jimmy Le Rouzic <jimmy.lerouzic@gmail.com>
2019-03-11 14:51:41 +0100

    ex

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'questions/answer'
+  # get 'questions/ask'
+  # get 'pages/ask'
+  # get 'pages/answer'
+     get 'ask', to: 'questions#ask', as: :ask
+     get 'answer', to: 'questions#answer', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
